7698c52ee9e7ff0785b0c5ea5f5e8cdcebc8f04a
zulmiross <zulejesus@hotmail.com>
2022-10-12 15:43:57 -0300

    refractor all

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b09464ce546e435c"; }
+/******/ 		__webpack_require__.h = function() { return "7eb649f0e34ff967"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
