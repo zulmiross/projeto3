96c17865c6e4f4de2c73c84517a7ac7ad93de677
zulmiross <zulejesus@hotmail.com>
2022-10-12 15:27:17 -0300

    update next

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Projeto 3</h1>
+    </div>
+  );
+}
+
+export default Home;
--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Projeto 3</h1>
-    </div>
-  );
-}
-
-export default App;
